fa68976a2830fa78ff29fee57295300f73dfcf65
Scott Cain <scain101@gmail.com>
2022-06-21 12:46:31 -0600

    Manipulating_Data_Lesson_06-21-22

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { companiesData } from "./companies-data";
-
-
-
+import { extraRecord } from "./extra-record";
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 //TODO: object functions.
 //TODO: Extract an element from the array.
@@ -16,6 +13,26 @@ before.innerHTML = JSON.stringify(companiesData, null, 2);
 //TODO: Create a new array with just elements of a certain state.
 //TODO: Create buttons for each record.
 //functions are here
-const manipulatedData = [];
+//const manipulatedData = companiesData.filter(function (company) {
+//return company.fieldData.State === "NM";
+//});
+const filterFunction = function (company) {
+  return company.fieldData.State === "TX";
+};
+
+const mappedFunction = function (company) {
+  return {
+    company: company.fieldData.CompanyName,
+    state: company.fieldData.State,
+  };
+};
+const manipulatedData = companiesData
+  .filter(filterFunction)
+  .map(mappedFunction);
+//ßconst manipulatedData = companiesData.map(mappedFunction);
+const after = document.getElementById("after");
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 //TODO: write manipulated data to the 'after' div.
+//slice is a way to remove elements out of the array
